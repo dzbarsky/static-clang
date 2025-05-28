b50d84918df31200a281fbb6d69a02a45eaf9cd4
David Zbarsky <dzbarsky@gmail.com>
2025-05-28 14:06:27 -0400

    Fix dist extensions

--- .github/workflows/build_release.sh
+++ .github/workflows/build_release.sh
@@ -2,16 +2,16 @@ set -eux
 
 bazel build --remote_header=x-buildbuddy-api-key="$BUILDBUDDY_API_KEY" //:for_all_platforms
 
-mv bazel-out/darwin_amd64-opt/bin/dist.tar.xz darwin_amd64.tar.xz
-mv bazel-out/darwin_amd64-opt/bin/dist_minimal.tar.xz darwin_amd64_minimal.tar.xz
+mv bazel-out/darwin_amd64-opt/bin/dist.tar.zst darwin_amd64.tar.zst
+mv bazel-out/darwin_amd64-opt/bin/dist_minimal.tar.zst darwin_amd64_minimal.tar.zst
 
-mv bazel-out/darwin_arm64-opt/bin/dist.tar.xz darwin_arm64.tar.xz
-mv bazel-out/darwin_arm64-opt/bin/dist_minimal.tar.xz darwin_arm64_minimal.tar.xz
+mv bazel-out/darwin_arm64-opt/bin/dist.tar.zst darwin_arm64.tar.zst
+mv bazel-out/darwin_arm64-opt/bin/dist_minimal.tar.zst darwin_arm64_minimal.tar.zst
 
-mv bazel-out/linux_arm64_musl-opt/bin/dist.tar.xz linux_arm64.tar.xz
-mv bazel-out/linux_arm64_musl-opt/bin/dist_minimal.tar.xz linux_arm64_minimal.tar.xz
+mv bazel-out/linux_arm64_musl-opt/bin/dist.tar.zst linux_arm64.tar.zst
+mv bazel-out/linux_arm64_musl-opt/bin/dist_minimal.tar.zst linux_arm64_minimal.tar.zst
 
-mv bazel-bin/dist.tar.xz linux_amd64.tar.xz
-mv bazel-bin/dist_minimal.tar.xz linux_amd64_minimal.tar.xz
+mv bazel-bin/dist.tar.zst linux_amd64.tar.zst
+mv bazel-bin/dist_minimal.tar.zst linux_amd64_minimal.tar.zst
 
-shasum -a 256 *tar.xz > SHA256.txt
+shasum -a 256 *tar.zst > SHA256.txt
